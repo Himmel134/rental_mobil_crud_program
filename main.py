7787d47e1aa92763712dde0d62f005d228b60715
jawed_iqbal <jawediqbalalfaruqiy>
2025-02-07 17:51:55 +0700

    rent retun

--- main.py
+++ main.py
@@ -7,18 +7,18 @@
 
 # /===== Imported file =====/
 from login_register import login, register
-from rent_return import status_cars, rented_cars, rent_car
+from rent_return import status_cars, rented_cars, rent_car, return_car
 
 # ===== Sign menu program =====
 def sign_menu():
-    option = None
-    while option != '3':
+    while True:
         print("\n=== RENTAL MOBIL HIMMEL ===")
         print("1. Masuk")
         print("2. Daftar")
         print("3. Exit") 
 
         option = input("Pilih opsi (1-3) : ")
+
         if option == '1':
             print("\n=== Masuk ===")
             print("Masukkan ID dan password")
@@ -30,6 +30,7 @@ def sign_menu():
                 return id_renter  # Mengembalikan ID pengguna setelah login sukses
             else:
                 print(user)  # Menampilkan pesan kesalahan
+
         elif option == '2':
             print("\n=== Daftar ===")
             print("Daftarkan ID dan password")
@@ -40,8 +41,10 @@ def sign_menu():
                 print("ID yang anda masukkan sudah ada sebelumnya")
             else:
                 print(user)
+
         elif option == '3':
-            print("\nGoodbye!")
+            print("\nTerima kasih telah menggunakan layanan kami!")
+            exit()
         else:
             print("Input invalid")
 
@@ -53,8 +56,7 @@ def main_menu(id_renter):
     #     print("Akses ditolak! Silakan login terlebih dahulu.")
     #     return
 
-    option = None
-    while option != '4':
+    while True:
         print(f"\nSelamat datang, {id_renter.capitalize()}!")  # Menampilkan ID pengguna
         print("\n=== MENU UTAMA ===")        
         print("1. Data mobil yang tersedia dan sewa Mobil")
@@ -63,6 +65,7 @@ def main_menu(id_renter):
         print("4. Exit")
 
         choice = input("Pilih opsi 1-4: ")
+
         if choice == '1':
             print("\nMobil yang tersedia")
         # Cek Ketersediaan mobil
@@ -75,21 +78,31 @@ def main_menu(id_renter):
                 car_id = input("\nMasukkan ID mobil yang ingin disewa : ").lower()
                 car_id = rent_car(car_id)
             elif want_rent.upper() == 'N':
-                return main_menu(id_renter)
+                continue
             else:
                 print("Harap masukkan input dengan benar")
+        
         elif choice == '2':
-            pass
+            car_id = input("Masukkan ID mobil yang ingin dikembalikan : ")
+            car_id = return_car(car_id)
+            print(car_id)
+            
         elif choice == '3':
             print("Kembali ke menu masuk")
-            return sign_menu()  # Kembali ke menu login
+            return
+        
         elif choice == '4':
             print("Terima kasih telah menggunakan layanan kami!")
-            break
+            exit()
+
         else:
             print("Input invalid")
 
+# ===== Main Loop =====
 if __name__ == "__main__":
-    id_renter = sign_menu()  # Menyimpan hasil login
-    if id_renter:  # Jika login berhasil
-        main_menu(id_renter)  # Masuk ke menu utama
+    while True:
+        id_renter = sign_menu()  # Menyimpan hasil login
+        if id_renter:  # Jika login berhasil
+            main_menu(id_renter)  # Masuk ke menu utama
+        else:
+            break
